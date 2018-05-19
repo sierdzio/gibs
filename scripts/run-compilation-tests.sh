fd3051f226dcaf2ce08da9c4bc31fa6fb8821776
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2018-05-19 13:10:59 +0200

    Add initial version of test project runner.

---
+++ scripts/run-compilation-tests.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Bail on errors.
+set -e
+
+if [ "${1}" = "-h" ] || [ "${1}" = "--help" ]; then
+  echo "Usage: run-compilation-tests.sh -q qt-directory -i ibs-exe-path "
+  echo "[-j jobs] [-m qmake-path]"
+  echo ""
+  echo "Compiles all tests located in ibs/testData using -j jobs, -q Qt directory,"
+  echo "-i ibs path and optional -m qmake path."
+  echo ""
+  echo "Where skin is optional. If specified, results will be limited to specified"
+  echo "skin and global changes."
+  exit
+fi
+
+JOBS="1"
+QTDIR=""
+IBSEXE=""
+QMAKEEXE=""
+
+while getopts "j:q:i:m" opt ;
+do
+  case $opt in
+  j) JOBS=$OPTARG
+    ;;
+  q) QTDIR=$OPTARG
+    ;;
+  i) IBSEXE=$OPTARG
+    ;;
+  m) QMAKEEXE=$OPTARG
+    ;;
+  :)
+    echo "Option -$OPTARG requires an argument."
+    exit 1
+    ;;
+  esac
+done
+
+for dir in ../testData/* ; do
+  cd $dir
+  echo "Entered: $dir"
+  echo "Cleanup"
+  # TODO: also remove moc def file
+  rm -f .ibs.cache *.o moc_* .qmake* Makefile
+  echo "IBS compiling: $dir/main.cpp"
+  time $IBSEXE -j $JOBS --qt-dir $QTDIR main.cpp
+  if [ ! -z "$QMAKEEXE" ]; then
+    echo "QMAKE compiling: $dir/main.cpp"
+    time $($QMAKEEXE; make -j $JOBS)
+  fi
+  rm -f .ibs.cache *.o moc_* .qmake* Makefile
+  cd ..
+done
+
+echo "Done"
