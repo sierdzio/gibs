502ebcb8f6eafd96230681dcab9b1f598c1edf3a
Tomasz Siekierda <sierdzio@gmail.com>
2025-05-30 17:41:45 +0200

    Simple stupid process impl.

--- libraries/process/stupidprocess.cpp
+++ libraries/process/stupidprocess.cpp
@@ -1,5 +1,26 @@
 #include "stupidprocess.h"
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string argsToString(const std::vector<std::string> &args)
+{
+    std::string result;
+
+    for (const auto &arg : args)
+    {
+        if (not result.empty())
+        {
+            result.append(" ");
+        }
+
+        result.append(arg);
+    }
+
+    return result;
+}
+
 void StupidProcess::setExecutable(const std::string &filePath)
 {
     this->filePath = filePath;
@@ -12,5 +33,6 @@ void StupidProcess::setArguments(const std::vector<std::string> &args)
 
 void StupidProcess::execute()
 {
-    // Sth.
+    std::cout << "Executing process: " << filePath << " with args: " << argsToString(args)
+              << std::endl;
 }
